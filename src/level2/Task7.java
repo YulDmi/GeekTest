42efbea73c6bf833777640f0d88ec5b454f6f3a9
YulDmi <barsetca@rambler.ru>
2020-01-18 11:15:20 +0300

    homeWork to lesson 2

---
+++ src/level2/Task7.java
@@ -0,0 +1,12 @@
+package level2;
+
+public class Task7 {
+    public static void main(String[] args) {
+        int [] d = {1, 5, 3, 2, 11, 4, -5, 2, 4, 8, 10, 1 };
+    }
+
+    public static void changePosition (int []arr, int n) {
+
+
+    }
+}
