ff173b0804cf7e3319d44e0af9758c5ea19fc16b
YulDmi <barsetca@rambler.ru>
2020-01-21 21:33:42 +0300

    game1_1

--- src/level3/Game.java
+++ src/level3/Game.java
@@ -7,47 +7,44 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Game {
-    static Random random = new Random();
+    Random random = new Random();
     char[] ch = new char[15];
-    static String[] words = {"apple", "orange", "lemon", "banana", "apricot",
+    private static String[] words = {"apple", "orange", "lemon", "banana", "apricot",
             "avocado", "broccoli", "carrot", "cherry", "garlic",
             "grape", "melon", "leak", "kiwi", "mango",
             "mushroom", "nut", "olive", "pea", "peanut",
             "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Game game = new Game();
-        int i = random.nextInt(20);
-        String puzzled = words[i];
         try {
-            game.start(puzzled);
+            game.start();
         } catch (IOException e) {
             System.out.println("что-то пошло не так");
         }
     }
 
-    public void start(String puzzled) throws IOException {
+    public void start() throws IOException {
         Arrays.fill(ch, '#');
+        String puzzled = words[random.nextInt(words.length)];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("What word is make?");
-        String str =  br.readLine();
-
+        System.out.println("What fruit or vegetable is made?");
+        System.out.println("To exit - ex");
+        String str = br.readLine();
         while (!str.equals(puzzled)) {
+            if (str.equals("ex")) return;
             print(str, puzzled);
             System.out.println("think more...");
             str = br.readLine();
         }
         System.out.println("You win!!!");
         System.out.println("Do you want to play more? 1 - yes, 0 - no ");
-        int answer = Integer.parseInt(br.readLine());
-        if (answer == 1) {
-
-            start(words[random.nextInt(20)]);
+        if (br.readLine().contains("1")) {
+            start();
         }
-
     }
 
-        public void print(String str, String puzzled) {
+    public void print(String str, String puzzled) {
         int size = Math.min(str.length(), puzzled.length());
         for (int i = 0; i < size; i++) {
             if (str.charAt(i) == puzzled.charAt(i)) ch[i] = str.charAt(i);
