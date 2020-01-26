2140dfec8b3144d12c6c55d056c054704222cf6a
YulDmi <barsetca@rambler.ru>
2020-01-26 17:28:09 +0300

    tictac без доработки интелекта

--- src/level4/TicTacToe.java
+++ src/level4/TicTacToe.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    private static final int SIZE = 4;
+    private static final int SIZE = 3;
     private static final int DOTS_TO_WIN = 3;
     private static final char DOT_EMPTY = '.';
     private static final char DOT_X = 'X';
     private static final char DOT_O = 'O';
     private static Random random = new Random();
     static Scanner scanner = new Scanner(System.in);
-
+    private static int steps = 0;
     private static char[][] map;
+    private static int count;
+
 
     public static void main(String[] args) {
         initial();
@@ -46,21 +48,23 @@ public class TicTacToe {
         do {
             System.out.println("Куда будем ставить X?");
             System.out.println("Введите номер строки: ");
-            rowNumber = scanner.nextInt();
+            rowNumber = scanner.nextInt() - 1;
             System.out.println("Введите номер столбца: ");
-            colNumber = scanner.nextInt();
-        } while (!isCellValid(rowNumber, colNumber));
-        map[rowNumber - 1][colNumber - 1] = DOT_X;
+            colNumber = scanner.nextInt() - 1;
+        } while (!isCellValid(rowNumber, colNumber, DOT_EMPTY));
+        map[rowNumber][colNumber] = DOT_X;
+        steps++;
     }
 
     private static void turnAi() {
+
         int rowNumber, colNumber;
         do {
             System.out.println(" ");
-            rowNumber = random.nextInt(SIZE) + 1;
-            colNumber = random.nextInt(SIZE) + 1;
-        } while (!isCellValid(rowNumber, colNumber));
-        map[rowNumber - 1][colNumber - 1] = DOT_O;
+            rowNumber = random.nextInt(SIZE);
+            colNumber = random.nextInt(SIZE);
+        } while (!isCellValid(rowNumber, colNumber, DOT_EMPTY));
+        map[rowNumber][colNumber] = DOT_O;
     }
 
     private static void print() {
@@ -88,63 +92,12 @@ public class TicTacToe {
         }
     }
 
-    private static boolean isCellValid(int rowNumber, int colNumber) {
-        if (rowNumber < 1 || rowNumber > SIZE)
+    private static boolean isCellValid(int rowNumber, int colNumber, char dot) {
+        if (rowNumber < 0 || rowNumber >= SIZE)
             return false;
-        if (colNumber < 1 || colNumber > SIZE)
+        if (colNumber < 0 || colNumber >= SIZE)
             return false;
-        return map[rowNumber - 1][colNumber - 1] == DOT_EMPTY;
-    }
-
-    public static boolean isWin(char dot) {
-        return winLine(dot) || winVertical(dot) || winDiagonal(dot);
-    }
-
-    public static boolean winDiagonal(char dot) {
-        int count = 0;
-
-        for (int i = 0; i < SIZE; i++) {
-            if (map[i][i] == dot) {
-                count++;
-                if (count == DOTS_TO_WIN) return true;
-            } else count = 0;
-        }
-        count = 0;
-        for (int i = 0; i < SIZE; i++) {
-            if (map[i][SIZE - 1 - i] == dot) {
-                count++;
-                if (count == DOTS_TO_WIN) return true;
-            } else count = 0;
-        }
-        return false;
-    }
-
-    private static boolean winVertical(char dot) {
-        int count;
-        for (int i = 0; i < SIZE; i++) {
-            count = 0;
-            for (int j = 0; j < SIZE; j++) {
-                if (map[j][i] == dot) {
-                    count++;
-                    if (count == DOTS_TO_WIN) return true;
-                } else count = 0;
-            }
-        }
-        return false;
-    }
-
-    public static boolean winLine(char dot) {
-        int count;
-        for (int i = 0; i < SIZE; i++) {
-            count = 0;
-            for (int j = 0; j < SIZE; j++) {
-                if (map[i][j] == dot) {
-                    count++;
-                    if (count == DOTS_TO_WIN) return true;
-                } else count = 0;
-            }
-        }
-        return false;
+        return map[rowNumber][colNumber] == dot;
     }
 
     public static boolean isMapFull() {
@@ -155,4 +108,89 @@ public class TicTacToe {
         }
         return true;
     }
+
+    public static boolean isWin(char dot) {
+        if (steps < DOTS_TO_WIN) return false;
+        for (int i = 0; i < SIZE; i++) {
+            count = 0;
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == dot) {
+                    if (getBooleanLine(dot, i, j)) {
+                        return true;
+                    }
+                    if (getBooleanVertical(dot, i, j)) {
+                        return true;
+                    }
+                    if (getBooleanRightDiagonal(dot, i, j)) {
+                        return true;
+                    }
+                    if (getBooleanLeftDiagonal(dot, i, j)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean getBooleanVertical(char dot, int i, int j) {
+        count++;
+        while (true) {
+            if (isCellValid(++i, j, dot)) {
+                count++;
+                if (count == DOTS_TO_WIN) {
+                    return true;
+                }
+            } else {
+                count = 0;
+                break;
+            }
+        }
+        return false;
+    }
+
+    public static boolean getBooleanLine(char dot, int i, int j) {
+        count++;
+        while (true) {
+            if (isCellValid(i, ++j, dot)) {
+                count++;
+                if (count == DOTS_TO_WIN) {
+                    return true;
+                }
+            } else {
+                count = 0;
+                break;
+            }
+        }
+        return false;
+    }
+
+    private static Boolean getBooleanLeftDiagonal(char dot, int i, int j) {
+        count++;
+        while (true) {
+            if (isCellValid(++i, --j, dot)) {
+                count++;
+                if (count == DOTS_TO_WIN) return true;
+            } else {
+                count = 0;
+                break;
+            }
+        }
+        return false;
+    }
+
+    private static Boolean getBooleanRightDiagonal(char dot, int i, int j) {
+        count++;
+        while (true) {
+            if (isCellValid(++i, ++j, dot)) {
+                count++;
+                if (count == DOTS_TO_WIN) return true;
+            } else {
+                count = 0;
+                break;
+            }
+        }
+        return false;
+    }
 }
+
